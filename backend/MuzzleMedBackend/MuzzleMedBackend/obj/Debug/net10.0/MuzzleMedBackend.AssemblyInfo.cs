6343bd62932d485f919f685040857515f2a73925
Paola Biscaro <paolabiscaroper@gmail.com>
2026-05-31 18:22:01 -0300

    chore: otimiza .gitignore e ajusta arquivos de ambiente

--- backend/MuzzleMedBackend/MuzzleMedBackend/obj/Debug/net10.0/MuzzleMedBackend.AssemblyInfo.cs
+++ backend/MuzzleMedBackend/MuzzleMedBackend/obj/Debug/net10.0/MuzzleMedBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MuzzleMedBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f5ef33181c1d7d6bdc0fbf46a51c62e3f2057b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b0fa23c5511668e9769765bb4ca7fe510a0f7ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MuzzleMedBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MuzzleMedBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
