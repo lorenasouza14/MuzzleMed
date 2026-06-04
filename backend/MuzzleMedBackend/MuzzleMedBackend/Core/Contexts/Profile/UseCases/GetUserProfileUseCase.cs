4f831d6d70d9e367496d26749f331897eb28d16a
Heloisa Ribeiro <hribe.silva@gmail.com>
2026-06-04 14:43:41 -0300

    feat: Get de usuarios

---
+++ backend/MuzzleMedBackend/MuzzleMedBackend/Core/Contexts/Profile/UseCases/GetUserProfileUseCase.cs
@@ -0,0 +1,33 @@
+﻿namespace MuzzleMedBackend.Core.Contexts.Profile.UseCases;
+
+using Domain.Contexts.Profile.Interfaces;
+using Core.Contexts.Profile.DTOs;
+
+public class GetUserProfileUseCase
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetUserProfileUseCase(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<UserResponse> ExecuteAsync(Guid userId)
+    {
+        var user = await _userRepository.GetByIdAsync(userId);
+
+        if (user == null)
+        {
+            throw new ArgumentException("Usuário não encontrado.");
+        }
+
+        return new UserResponse(
+            user.Id,
+            user.FullName,
+            user.ProfileEmail.Address, 
+            user.Cpf.Number,
+            user.Phone.Number,
+            user.DateOfBirth
+        );
+    }
+}
