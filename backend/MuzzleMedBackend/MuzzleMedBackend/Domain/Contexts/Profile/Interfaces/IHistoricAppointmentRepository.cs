0fdce236ab1ccd70ee5f4a151afe9d7c25f2a6f6
Heloisa Ribeiro <hribe.silva@gmail.com>
2026-06-04 15:54:53 -0300

    feat: get da tabela HistoricAppointment

---
+++ backend/MuzzleMedBackend/MuzzleMedBackend/Domain/Contexts/Profile/Interfaces/IHistoricAppointmentRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace MuzzleMedBackend.Domain.Contexts.Profile.Interfaces;
+
+using Domain.Contexts.Profile.Entities;
+
+public interface IHistoricAppointmentRepository
+{
+    Task<IEnumerable<HistoricAppointment>> GetByPetIdAsync(Guid petId);
+}
---
+++ backend/MuzzleMedBackend/MuzzleMedBackend/Infrastructure/Contexts/Profile/Repositories/HistoricAppointmentRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace MuzzleMedBackend.Infrastructure.Contexts.Profile.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using Domain.Contexts.Profile.Entities;
+using Domain.Contexts.Profile.Interfaces;
+
+public class HistoricAppointmentRepository : IHistoricAppointmentRepository
+{
+    private readonly AppDbContext _context;
+
+    public HistoricAppointmentRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<HistoricAppointment>> GetByPetIdAsync(Guid petId)
+    {
+        return await _context.Set<HistoricAppointment>()
+            .AsNoTracking()
+            .Where(h => h.PetId == petId)
+            .OrderByDescending(h => h.Date)
+            .ToListAsync();
+    }
+}
