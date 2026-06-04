18a03021363d0472af2a2d2198924d5cd1751bf8
Heloisa Ribeiro <hribe.silva@gmail.com>
2026-06-04 14:35:01 -0300

    feat: Get de pets

---
+++ backend/MuzzleMedBackend/MuzzleMedBackend/Core/Contexts/Profile/DTOs/PetResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MuzzleMedBackend.Core.Contexts.Profile.DTOs;
+
+using Domain.Contexts.Profile.ValueObjects;
+
+public record PetResponse(
+    Guid Id,
+    string Name,
+    SpecieEnum Specie,
+    string Breed,
+    DateOnly DateOfBirth,
+    GenderEnum Gender
+);
---
+++ backend/MuzzleMedBackend/MuzzleMedBackend/Core/Contexts/Profile/UseCases/GetPetsByUserUseCase.cs
@@ -0,0 +1,28 @@
+﻿namespace MuzzleMedBackend.Core.Contexts.Profile.UseCases;
+
+using Domain.Contexts.Profile.Interfaces;
+using Core.Contexts.Profile.DTOs;
+
+public class GetPetsByUserUseCase
+{
+    private readonly IPetRepository _petRepository;
+
+    public GetPetsByUserUseCase(IPetRepository petRepository)
+    {
+        _petRepository = petRepository;
+    }
+
+    public async Task<IEnumerable<PetResponse>> ExecuteAsync(Guid userId)
+    {
+        var pets = await _petRepository.GetActivePetsByUserIdAsync(userId);
+
+        return pets.Select(pet => new PetResponse(
+            pet.Id,
+            pet.Name,
+            pet.Specie,
+            pet.Breed,
+            pet.DateOfBirth,
+            pet.Gender
+        ));
+    }
+}
