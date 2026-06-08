08c96c59b67ed9afc5ce34ffe9fe7b7de4b98b07
Lucas Hygidio <lucashygidio123@gmail.com>
2026-06-08 03:07:24 -0300

    Adicionando UnitOfWork ao criar historico de appointment

--- backend/MuzzleMedBackend/MuzzleMedBackend/Domain/Contexts/Schedule/Interfaces/UseCases/IBuildAppointmentResponseUseCase.cs
+++
@@ -1,9 +0,0 @@
-using MuzzleMedBackend.Core.Contexts.Schedule.DTOs;
-using MuzzleMedBackend.Domain.Contexts.Schedule.Entities;
-
-namespace MuzzleMedBackend.Domain.Contexts.Schedule.Interfaces.UseCases;
-
-public interface IBuildAppointmentResponseUseCase
-{
-    Task<AppointmentScheduleResponseDto> ExecuteAsync(AppointmentScheduleContext appointment);
-}
